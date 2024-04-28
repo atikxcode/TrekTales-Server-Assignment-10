4a51284171c41314b5021e81f656f2413b3c01dd
atikxcode <liadbd20@gmail.com>
2024-04-29 00:05:29 +0600

    delete functionality in backend added

--- index.js
+++ index.js
@@ -92,6 +92,15 @@ async function run() {
 
 
 
+     app.delete('/touristspot/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await touristCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
 
 
     // Country 
